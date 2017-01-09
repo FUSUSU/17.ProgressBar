5d63c5bfcfb3e67814f53222c6c8964401f17eec
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-01-09 19:37:38 +0700

    Khai báo các biến

--- app/src/main/java/com/example/teo/a17progressbar/MainActivity.java
+++ app/src/main/java/com/example/teo/a17progressbar/MainActivity.java
@@ -1,9 +1,23 @@
 package com.example.teo.a17progressbar;
 
+import android.app.ProgressDialog;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+    //khai báo Button Bắt đầu tiến trình
+    Button btnStartProgress;
+    //Khai báo thanh tiến trình trình
+    ProgressDialog progressBar;
+    //Khai báo biến tiến trình đang bao nhiêu?
+    private int progressBarStatus = 0;
+    //Biến xử lý.
+    private Handler progressBarHandler = new Handler();
+        //Biến lưu kích thước của file
+        private long fileSize = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
